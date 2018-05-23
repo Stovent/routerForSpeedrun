407e9b9984f5e7b64d50b75c01bd96d0c6a534d6
Stovent <Stovent@users.noreply.github.com>
2018-05-23 13:47:36 +0200

    file reader and beginning of calculator

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,9 @@
+#include <algorithm>
 #include <iostream>
-#include <map>
+#include <fstream>
 #include <string>
+#include <vector>
+#include <map>
 
 #include "Step.hpp"
 
@@ -14,7 +17,45 @@ int main(int argc, char* argv[])
         return 1;
     }
     
-    map<string, Step> steps;
+    
+    std::vector<std::string> route; // final route
+    std::map<string, Step> steps;
+    
+    std::ifstream input(argv[1]);
+    std::string tmp, tmpName;
+    int tmpArg = 0;
+    
+    
+    // read input file and create steps
+    while(input.good())
+    {
+        input >> tmpName;
+        steps.insert(std::pair<std::string, Step>(tmpName, Step()));
+        cin >> tmpArg;
+        while(tmpArg)
+        {
+            cin >> tmp;
+            steps[tmpName].AddPrerequisite(tmp);
+            tmpArg--;
+        }
+    }
+    
+    
+    // calculates the route
+    while(route.size() < steps.size())
+    {
+        for(std::map<string, Step>::iterator it = steps.begin(); it != steps.end(); it++)
+        {
+            if(find(route.begin(), route.end(), it->first) == route.end()) // the step is not in the route so we add it
+            {
+                if(it->second.IsUsable())
+                {
+                    route.push_back(it->first);
+                    it->second.Usable(false);
+                }
+            }
+        }
+    }
     
     return 0;
 }
