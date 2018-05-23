2c5168649a3e1aff58ae5452325f840f8147890d
Stovent <Stovent@users.noreply.github.com>
2018-05-23 11:21:34 +0200

    can only be used with a file now

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,20 @@
+#include <iostream>
+#include <map>
+#include <string>
 
+#include "Step.hpp"
+
+using namespace std;
 
 int main(int argc, char* argv[])
 {
+    if(argc != 2)
+    {
+        cout << "This program need 1 and only 1 file!" << std::endl;
+        return 1;
+    }
+    
+    map<string, Step> steps;
+    
     return 0;
 }
